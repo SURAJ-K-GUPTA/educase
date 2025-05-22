03f91fc578783550b9c3a711727f54f4d9a601ae
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2025-05-23 01:52:26 +0530

    `Update index.js and package.json with minor changes`

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
   res.json({ message: "ok" });
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at Port ${port}`);
 });
